5775fd7ff16337818bfb7044c33f0a9004c24ee5
Audrey <a.chobert@e-cobot.com>
2020-06-10 17:10:03 +0200

    2em

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Audrey"/>
+    <LoginForm msg="Identifiez vous"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import LoginForm from '@/components/LoginForm.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    LoginForm,
   }
 }
 </script>
